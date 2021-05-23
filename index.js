c576e7f4c8df788bc2ccb172f80957b8db0f5ee3
mensha <msksalston@gmail.com>
2021-05-23 16:13:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driverArray, string) {
+  
+    const result = driverArray.filter(driver => driver.name.startsWith `${string}`);
+    return result
+}
+    // var n = str.startsWith("Hello");
+
+// const result = words.filter(word => word.length > 6);
+
+// console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
+
